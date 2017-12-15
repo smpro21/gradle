f317595572104ba88dd15ccc36fffc8b806fef89
Cedric Champeau <cedric@gradle.com>
2017-12-15 10:23:56 +0100

    Initial implementation of component metadata rules allowing modification of variant attributes
    This commit introduces metadata rules that support modification of variant attributes. Variant attributes
    are specific to each variant and can be found in module metadata. Those are NOT component level attributes,
    which could be used during dependency resolution. This will be added in a subsequent commit.

---
+++ subprojects/dependency-management/src/testFixtures/groovy/org/gradle/integtests/fixtures/publish/VariantSpec.groovy
@@ -0,0 +1,44 @@
+/*
+ * Copyright 2017 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.gradle.integtests.fixtures.publish
+
+import groovy.transform.CompileStatic
+
+@CompileStatic
+class VariantSpec {
+    String name
+    Map<String, String> attributes = [:]
+    List<ArtifactSpec> artifacts = []
+
+    void attribute(String name, String value) {
+        attributes[name] = value
+    }
+
+    void artifact(String name) {
+        artifacts << new ArtifactSpec(name: name)
+    }
+
+    void artifact(String name, String url) {
+        artifacts << new ArtifactSpec(name: name, url: url)
+    }
+
+    static class ArtifactSpec {
+        String name
+        String url
+        String ext = 'jar'
+    }
+}
